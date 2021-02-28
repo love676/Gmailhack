cbe337702555b4d06a20beacfde7f2ba3fe56ade
Love <78062729+love676@users.noreply.github.com>
2021-02-28 19:03:55 +0630

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+<p>git clone github.com:love676/Gmailhack.git
+<p>cd Gmailhack
+<p>python gmail.hack.py
---
+++ gmail.hack.py
@@ -0,0 +1,32 @@
+import sys
+import os
+import time
+import socket
+import random
+id = input(" Target gmail: ")
+os.system("clear")
+print ("♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥	 wait Hack password♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥♥")
+time.sleep(10)
+print ("gmail password is wait --------> 0% ")
+time.sleep(5)
+print ("gmail password is wait --------> 10%")
+time.sleep(5)
+print ("gmail password is wait --------> 20% ")
+time.sleep(5)
+print ("gmail password is wait --------> 35% ")
+time.sleep(5)
+print ("gmail password is wait --------> 50% ")
+time.sleep(5)
+print ("gmail password is wait --------> 65% ")
+time.sleep(5)
+print ("gmail password is wait --------> 75% ")
+time.sleep(5)
+print ("gmail password is wait --------> 85% ")
+time.sleep(5)
+print ("gmail password is wait --------> 99% ")
+time.sleep(5)
+print ("gmail password is wait --------> 100% ")
+time.sleep(5)
+os.system("clear")
+print ("gmail password is = Leee pay ya mal")
+
